b33a5909db8db7f5ef63515fbf55305f100368f4
Ripra87 <83763060+Ripra87@users.noreply.github.com>
2023-09-03 18:07:23 +0200

    Update challenge.js

--- modules/challenge.js
+++ modules/challenge.js
@@ -1,26 +1,27 @@
 function typing (e, textInput, result, wordsArea ) {
-    
-    if (e.key === "Enter" && textInput.value == result.words[result.counter]) {
-        wordsArea.children[result.counter].style.color = "green";
-        result.counter++;
-        result.right++;
-        textInput.value="";
-        for (let i = 0; i < wordsArea.children.length; i++) {
-            wordsArea.children[i].style.backgroundColor = "white";
+    if (result.start) {
+        if (e.key === "Enter" && textInput.value == result.words[result.counter]) {
+            wordsArea.children[result.counter].style.color = "green";
+            result.counter++;
+            result.right++;
+            textInput.value="";
+            for (let i = 0; i < wordsArea.children.length; i++) {
+                wordsArea.children[i].style.backgroundColor = "white";
+            }
+            wordsArea.children[result.counter].style.backgroundColor = "yellow";
+        } 
+        else if (e.key === "Enter" && textInput.value !== result.words[result.counter]) {
+            wordsArea.children[result.counter].style.color = "blue";
+            result.counter++;
+            result.wrong++;
+            textInput.value= "";
+            for (let i = 0; i < wordsArea.children.length; i++) {
+                wordsArea.children[i].style.backgroundColor = "white";
+            }
+            wordsArea.children[result.counter].style.backgroundColor = "yellow";
+            
         }
-        wordsArea.children[result.counter].style.backgroundColor = "yellow";
-    } else if (e.key === "Enter" && textInput.value !== result.words[result.counter]) {
-        wordsArea.children[result.counter].style.color = "blue";
-        result.counter++;
-        result.wrong++;
-        textInput.value= "";
-        for (let i = 0; i < wordsArea.children.length; i++) {
-            wordsArea.children[i].style.backgroundColor = "white";
-        }
-        wordsArea.children[result.counter].style.backgroundColor = "yellow";
-        
     }
-    
 }
 
 
