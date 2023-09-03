5f10d9c46938f0d5b7adb566bfc802c4a3634334
Ripra87 <83763060+Ripra87@users.noreply.github.com>
2023-09-03 18:05:55 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -13,29 +13,35 @@ const resultArea = document.getElementById("result");
 const correctAnswers = document.getElementById("correct");
 const wrongAnswers = document.getElementById("wrong");
 
-const result = { counter: 0, right: 0, wrong: 0, words: [] };
+const result = { counter: 0, right: 0, wrong: 0, words: [], testStart: false };
 
 document.addEventListener("DOMContentLoaded",() => {
     fillWordsArea(wordsArea, textInput, timeLeft, wordsGroup, result, resultArea);
-})
+});
+
 
 newTest.addEventListener("click", () => {
     fillWordsArea(wordsArea, textInput, timeLeft, wordsGroup, result, resultArea);
-    
-
 });
 
 
 textInput.addEventListener("click", () => {
-    wordsArea.children[0].style.backgroundColor = "yellow"
-    let timer = setInterval(() => {
-        countdown(timeLeft);
-        if (timeLeft.innerText <= 0) {
-            clearInterval(timer);
-            showResult(resultArea, correctAnswers, wrongAnswers, result);
-        };
-    }, 1000);
-    
+    if (!result.testStart) {
+        result.testStart = true;
+        wordsArea.children[0].style.backgroundColor = "yellow"
+        let timer = setInterval(() => {
+            if ( !result.testStart ) {
+                clearInterval(timer);
+            } else {
+                countdown(timeLeft);
+                if (timeLeft.innerText <= 0 ) {
+                    clearInterval(timer);
+                    showResult(resultArea, correctAnswers, wrongAnswers, result);
+                    result.testStart = false;
+                };
+            };
+        }, 1000);
+    };      
 });
 
 
